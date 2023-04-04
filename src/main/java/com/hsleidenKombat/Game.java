b7da31b2112d1094e3d3d505ad1b93ee44ce4fd4
Hichamspk <s1143822@student.hsleiden.nl>
2023-04-04 12:40:08 +0200

    Versie 1 New Movement Animatie Physics(werkt niet helemaal)

--- src/main/java/com/hsleidenKombat/Game.java
+++ src/main/java/com/hsleidenKombat/Game.java
@@ -6,6 +6,8 @@ import com.almasb.fxgl.core.asset.AssetLoaderService;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.CollisionHandler;
 import com.almasb.fxgl.physics.HitBox;
@@ -29,15 +31,15 @@ public class Game extends GameApplication {
     @Override
     protected void initGame() {
 
-         player1 = FXGL.entityBuilder()
+        player1 = FXGL.entityBuilder()
                 .at(100, 300)
                 .with(new AnimationComponent())
-                 .with(new CollidableComponent(true))
-                 .type(EntityTypes.PLAYER1)
+                .with(new CollidableComponent(true))
+                .type(EntityTypes.PLAYER1)
                 .buildAndAttach();
 
-
         player1.setScaleY(2.0);
+        player1.addComponent(new PlayerComponent());
 
         player2 = FXGL.entityBuilder()
                 .at(700, 300)
@@ -47,23 +49,88 @@ public class Game extends GameApplication {
                 .buildAndAttach();
 
         player2.setScaleY(2.0);
+        player2.addComponent(new Player2Component());
 
-        Player1Controls player1Controls = new Player1Controls(player1, KeyCode.A, KeyCode.D, KeyCode.S, KeyCode.W);
-        player1Controls.init(getInput());
+        Input input = getInput();
 
-        Player2Controls player2Controls = new Player2Controls(player2, KeyCode.LEFT, KeyCode.RIGHT, KeyCode.DOWN, KeyCode.UP);
-        player2Controls.init(getInput());
-    }
-
-    protected void initPhysics(){
-        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityTypes.PLAYER1, EntityTypes.PLAYER2) {
+// Bewegingskeybinds voor player1
+        input.addAction(new UserAction("Move Left") {
             @Override
-            protected void onCollision(Entity player1, Entity player2) {
-                player2.removeFromWorld();
+            protected void onAction() {
+                player1.getComponent(PlayerComponent.class).left();
             }
-        });
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                player1.getComponent(PlayerComponent.class).right();
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                player1.getComponent(PlayerComponent.class).jump();
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Duck") {
+            @Override
+            protected void onAction() {
+                player1.getComponent(PlayerComponent.class).down();
+            }
+
+            protected void onActionEnd() {
+                player1.getComponent(PlayerComponent.class).downReleased();
+            }
+        }, KeyCode.S);
+
+
+        // Bewegingskeybinds voor player2
+        input.addAction(new UserAction("Move Left 2") {
+            @Override
+            protected void onAction() {
+                player2.getComponent(Player2Component.class).left2();
+            }
+        }, KeyCode.LEFT);
+
+        input.addAction(new UserAction("Move Right 2") {
+            @Override
+            protected void onAction() {
+                player2.getComponent(Player2Component.class).right2();
+            }
+        }, KeyCode.RIGHT);
+
+        input.addAction(new UserAction("Move Up 2") {
+            @Override
+            protected void onAction() {
+                player2.getComponent(Player2Component.class).jump2();
+            }
+        }, KeyCode.UP);
+
+
+        input.addAction(new UserAction("Duck2") {
+            @Override
+            protected void onAction() {
+                player2.getComponent(Player2Component.class).down2();
+            }
+
+            protected void onActionEnd() {
+                player2.getComponent(Player2Component.class).downReleased();
+            }
+        }, KeyCode.DOWN);
     }
-    public static void main(String[] args) {
-        launch(args);
+
+        protected void initPhysics () {
+            FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityTypes.PLAYER1, EntityTypes.PLAYER2) {
+                @Override
+                protected void onCollision(Entity player1, Entity player2) {
+                    player2.removeFromWorld();
+                }
+            });
+        }
+        public static void main (String[]args){
+            launch(args);
+        }
     }
-}
---
+++ src/main/java/com/hsleidenKombat/Player2Component.java
@@ -0,0 +1,61 @@
+package com.hsleidenKombat;
+
+import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.entity.components.TransformComponent;
+import com.almasb.fxgl.input.Input;
+
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getInput;
+
+public class Player2Component extends Component {
+    private TransformComponent position;
+    private double speed = 0;
+    private double jumpSpeed = 0;
+    private boolean isJumping = false;
+    private boolean isDucking = false;
+    private double gravity = 2500;
+
+    @Override
+    public void onUpdate(double tpf) {
+        speed = tpf * 60;
+        if (isJumping) {
+            position.translateY(-jumpSpeed * tpf);
+            jumpSpeed -= gravity * tpf;
+            if (position.getY() >= 300) {
+                if(!isDucking) {
+                    isJumping = false;
+                    position.setY(300);
+                }
+            }
+        }
+    }
+
+    public void jump2() {
+        if (!isJumping && !isDucking) {
+            jumpSpeed = 600;
+            isJumping = true;
+        }
+    }
+    public void down2() {
+        if (!isJumping && !isDucking) {
+            isDucking = true;
+            position.setScaleY(1.0);
+            position.setY(333);
+        }
+    }
+
+    public void downReleased() {
+        if (isDucking) {
+            isDucking= false;
+            position.setScaleY(2.0);
+            position.setY(300);
+        }
+    }
+
+
+    public void left2(){
+        position.translateX(-5 * speed);
+    }
+    public void right2(){
+        position.translateX(5 * speed);
+    }
+}
