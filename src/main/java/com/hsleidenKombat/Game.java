4cd6af91f14b56df3a112866e501045d241fc9f8
talonhbu <129837789+talonhbu@users.noreply.github.com>
2023-04-05 16:10:52 +0200

    + loginpagina

--- src/main/java/com/hsleidenKombat/Game.java
+++ src/main/java/com/hsleidenKombat/Game.java
@@ -19,8 +19,14 @@ import com.almasb.fxgl.physics.PhysicsComponent;
 import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
 import com.sun.javafx.geom.Point2D;
 import com.sun.javafx.geom.Rectangle;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -29,6 +35,10 @@ import static com.almasb.fxgl.dsl.FXGLForKtKt.*;
 public class Game extends GameApplication {
 
     private Entity player1, player2, punch;
+    private TextField player1NameField, player2NameField;
+    private Button startButton;
+
+    private Text title;
 
 
     @Override
@@ -43,7 +53,8 @@ public class Game extends GameApplication {
 
     @Override
     protected void initGame() {
-        getGameScene().setBackgroundRepeat("Background7.jpeg");
+        getGameScene().setBackgroundRepeat("login.jpeg");
+        createMenu();
         FactoryComponent factoryComponent = new FactoryComponent();
         player1 = factoryComponent.spawnPlayer1(100, 300);
         getGameWorld().addEntity(player1);
@@ -120,7 +131,77 @@ public class Game extends GameApplication {
 
     }
 
+    private void createMenu() {
+        VBox menuBox = new VBox(10);
+        StackPane stackPane = new StackPane();
+        stackPane.setPrefSize(getAppWidth(), getAppHeight());
 
+        title = new Text("Hsleiden Kombat");
+        title.setFont(Font.font("Verdana", 40));
+        title.setFill(Color.BLACK);
+        title.setText(title.getText().toUpperCase());
+
+        stackPane.getChildren().add(title);
+        getGameScene().addUINode(stackPane);
+
+
+        menuBox.setTranslateX(getAppWidth() / 2 - 100);
+        menuBox.setTranslateY(getAppHeight() / 2 - 100);
+
+//        player2.setX(500);
+//        player1.setX(-500);
+
+        player1NameField = new TextField();
+        player1NameField.setPromptText("Speler 1 naam");
+        player1NameField.setMaxWidth(200);
+
+        player2NameField = new TextField();
+        player2NameField.setPromptText("Speler 2 naam");
+        player2NameField.setMaxWidth(200);
+
+        startButton = new Button("Beginnen");
+        startButton.setMaxWidth(200);
+
+        menuBox.getChildren().addAll(title, player1NameField, player2NameField, startButton);
+        getGameScene().addUINode(menuBox);
+
+        startButton.setOnAction(e -> {
+            String player1Name = player1NameField.getText();
+            String player2Name = player2NameField.getText();
+
+            if (!player1Name.isEmpty() && !player2Name.isEmpty()) {
+                // Doel: start het spel en verberg het menu scherm
+                startGame();
+            }
+        });
+    }
+
+    private void startGame() {
+        // Verberg het menu scherm
+        this.title.setVisible(false);
+        player1NameField.setVisible(false);
+        player2NameField.setVisible(false);
+        startButton.setVisible(false);
+        getGameScene().setBackgroundRepeat("background7.jpeg");
+
+        startlevel2();
+    }
+
+    private void startlevel2(){
+        this.title.setVisible(false);
+        player1NameField.setVisible(false);
+        player2NameField.setVisible(false);
+        startButton.setVisible(false);
+        getGameScene().setBackgroundRepeat("background7.jpeg");
+    }
+
+    private void startlevel3(){
+        this.title.setVisible(false);
+        player1NameField.setVisible(false);
+        player2NameField.setVisible(false);
+        startButton.setVisible(false);
+        getGameScene().setBackgroundRepeat("background7.jpeg");
+    }
 
 
 
